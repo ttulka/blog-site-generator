607e82e93a9110d5cb3eaecc7854c3a89e41fe2e
Tomas Tulka <dev@ttulka.com>
2022-10-31 13:43:36 +0100

    update to jdk 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     application
 }
 
@@ -16,16 +16,17 @@ dependencies {
     implementation("com.sksamuel.hoplite:hoplite-yaml:2.6.2")
     implementation("com.sksamuel.hoplite:hoplite-json:2.6.2")
     testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
-}
-
 application {
     mainClass.set("com.ttulka.blog.jamstack.ApplicationKt")
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "17"
 }
